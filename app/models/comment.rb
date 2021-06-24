9aba47ef319c3e8f86343cfd5de4181d1c70766f
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-06-24 11:29:26 -0400

    commit tree 1a4d74646169fb363272adab8d90bb33f8b0e606

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,16 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+
+
+  def user_attributes=(user_attributes)
+    if !user_attributes[:username].empty?
+      user = User.create(user_attributes)
+      self.user_id = user.id
+    else
+      false
+    end
+
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,19 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
-  has_many :categories, through: :post_categories
+  has_many :categories, through: :post_categories #this adds the category_ids method which allows category_id in an array
   has_many :comments
   has_many :users, through: :comments
+
+  def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, category_attributes|
+      if !category_attributes[:name].empty?
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
+    end
+  end
 
 
 end
